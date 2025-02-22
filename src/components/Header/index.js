4feca426c0d8575c57d3914cf7108edea0618dfa
luigiliezz <luizantonioatercio@gmail.com>
2025-02-22 12:17:56 -0300

    oi

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,55 +1,99 @@
 'use client';
+
 import Link from "next/link"
 import React, { useState } from 'react';
 import styles from "./Header.module.css";
-import Image from "next/image";
+import Image from "next/image"
 
 const Header = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
+  const toggleMenu = () => {
 
-        setIsMenuOpen(!isMenuOpen);
-    };
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    return (
-        <header className={styles.header}>
+  return (
+    <header className={styles.header}>
 
-            <div className={styles.logo}>
-                <Image className={styles.img} src='/images/clinic.gif' alt=" logo " width={50} height={150} />
-                
-            </div>
+      <div className={styles.logo}>
+        <Image className={styles.img} src="/images/relatorio-de-saude.gif" alt="Logo" width={50} height={80} />
+      </div>
 
-            {isMenuOpen &&
-                <nav className={`${styles.nav} ${isMenuOpen ? styles.open : ''}`}>
-                    <ul className={styles.lista}>
-                        <li>
-                            <Link className={styles.link} href='/'>Home</Link>
-                        </li>
+      {isMenuOpen &&
+        <nav className={`${styles.nav} ${isMenuOpen ? styles.open : ''}`}>
+          <ul className={styles.lista}>
 
-                        <li>
-                            <Link className={styles.link} href="/sobre">Médicos</Link>
-                        </li>
+            <li>
+              <Link className={styles.link} href='/'>Home</Link>
+            </li>
 
-                        <li>
-                            <Link className={styles.link} href="/sobre">Pacientes</Link>
-                        </li>
+            <li class={styles.li}><Link className={styles.link} href="/medicos">Médicos</Link>
+              <ul class={styles.ulSubmenu}>
+                <li class={styles.lisubmenu}>
+                  <Link class={styles.linkSmenu} href="/medicos/listar">Listar</Link>
+                </li>
+                <li class={styles.lisubmenu}>
+                  <Link class={styles.linkSmenu} href="/medicos/adicionar">Adicionar</Link>
+                </li>
+                <li class={styles.lisubmenu}>
+                  <Link class={styles.linkSmenu} href="/medicos/editar">Editar</Link>
+                </li>
+                <li class={styles.lisubmenu}>
+                  <Link class={styles.linkSmenu} href="/medicos/excluir">Excluir</Link>
+                </li>
+              </ul>
+            </li>
 
-                        <li>
-                            <Link className={styles.link} href="/sobre">Agendamentos</Link>
-                        </li>
 
-                    </ul>
-                </nav>
-            }
-            <button className={styles.menuButton} onClick={toggleMenu}>
-                {isMenuOpen ? 'Fechar' : 'Menu'}
-            </button>
-        </header>
-    );
+            <li class={styles.li}><Link className={styles.link} href="/pacientes">Pacientes</Link>
+              <ul class={styles.ulSubmenu}>
+                <li class={styles.lisubmenu}>
+                  <Link class={styles.linkSmenu} href="/pacientes/listar">Listar</Link>
+                </li>
+                <li class={styles.lisubmenu}>
+                  <Link class={styles.linkSmenu} href="/pacientes/adicionar">Adicionar</Link>
+                </li>
+                <li class={styles.lisubmenu}>
+                  <Link class={styles.linkSmenu} href="/pacientes/editar">Editar</Link>
+                </li>
+                <li class={styles.lisubmenu}>
+                  <Link class={styles.linkSmenu} href="/pacientes/excluir">Excluir</Link>
+                </li>
+              </ul>
+            </li>
+
+
+            <li class={styles.li}><Link className={styles.link} href="/agendamento">Agendamento</Link>
+              <ul class={styles.ulSubmenu}>
+                <li class={styles.lisubmenu}>
+                  <Link class={styles.linkSmenu} href="/agendamento/listar consultas">Listar consultas</Link>
+                </li>
+                <li class={styles.lisubmenu}>
+                  <Link class={styles.linkSmenu} href="/agendamento/adicionar consulta">Adicionar consulta</Link>
+                </li>
+                <li class={styles.lisubmenu}>
+                  <Link class={styles.linkSmenu} href="/agendamento/editar agendamento">Editar agendamento</Link>
+                </li>
+                <li class={styles.lisubmenu}>
+                  <Link class={styles.linkSmenu} href="/agendamento/cancelar">Cancelar</Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      }
+      <button className={styles.menuButton} onClick={toggleMenu}>
+        {isMenuOpen ? 'Fechar' : 'Menu'}
+      </button>
+    </header>
+  );
 };
 
 export default Header;
+
+
+
 
 
 /*export default function Header(){
